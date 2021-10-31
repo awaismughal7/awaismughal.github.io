9e2874f90480fc9a75b39f93ab9daa73c282d56f
awaismughal7 <93459786+awaismughal7@users.noreply.github.com>
2021-10-30 22:09:45 -0700

    Awais Mughal Website
    this website is made for source code,

---
+++ files/Scripts/index.js
@@ -0,0 +1,18 @@
+var visitor = "you visited this website at";
+document.write(visitor);
+document.write(" <br> ");
+
+let date = new Date();
+document.write(date.getDay());
+document.write("-");
+document.write(date.getMonth());
+document.write("-");
+document.write(date.getFullYear());
+document.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+document.write(date.getHours());
+document.write(":");
+document.write(date.getMinutes());
+document.write(":");
+document.write(date.getSeconds());
+document.write(":");
+document.write(date.getMilliseconds());
